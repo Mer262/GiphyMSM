fb56a202270f4b52e04ce79fc40c978e3906defe
Meredith Moore <meredithmoore@Merediths-MacBook-Pro.local>
2017-01-16 21:37:43 -0500

    fixes

--- assets/javascript/giphycheck.js
+++ assets/javascript/giphycheck.js
@@ -1,95 +1,100 @@
-// 	* Be sure to read about these GIPHY parameters (hint, hint): 
-//		* `q` - search query term or phrase
-//		* `limit` - (optional) number of results to return, 
-//			maximum 100. Default 25.
-//		* `rating` - (optional) limit results to those rated 
-//			(y,g, pg, pg-13 or r).
+//  * Be sure to read about these GIPHY parameters (hint, hint): 
+//      * `q` - search query term or phrase
+//      * `limit` - (optional) number of results to return, 
+//          maximum 100. Default 25.
+//      * `rating` - (optional) limit results to those rated 
+//          (y,g, pg, pg-13 or r).
 // 1. Before you can make any part of our site work, you need
-//	to create an array of strings, each one related to a topic
-// 	that interests you. Save it to a variable called `topics`. 
-//		* We chose animals for our theme, but you can make a list
-//		to your own liking.
+//  to create an array of strings, each one related to a topic
+//  that interests you. Save it to a variable called `topics`. 
+//      * We chose animals for our theme, but you can make a list
+//      to your own liking.
 
 // 2. Your app should take the topics in this array and create 
-//	buttons in your HTML.
-//		* Try using a loop that appends a button for each string 
-//		in the array.
+//  buttons in your HTML.
+//      * Try using a loop that appends a button for each string 
+//      in the array.
 
 // 3. When the user clicks on a button, the page should grab 10
-//	static, non-animated gif images from the GIPHY API and place them 
-//	on the page. 
+//  static, non-animated gif images from the GIPHY API and place them 
+//  on the page. 
 
 // 4. When the user clicks one of the still GIPHY images, the gif 
-//	should animate. If the user clicks the gif again, it should stop playing.
+//  should animate. If the user clicks the gif again, it should stop playing.
 
 // 5. Under every gif, display its rating (PG, G, so on). 
-//		* This data is provided by the GIPHY API.
-//		* Only once you get images displaying with button presses should 
-//		you move on to the next step.
+//      * This data is provided by the GIPHY API.
+//      * Only once you get images displaying with button presses should 
+//      you move on to the next step.
 
 // 6. Add a form to your page takes the value from a user input box 
-//	and adds it into your `topics` array. Then make a function call 
-//	that takes each topic in the array remakes the buttons on the page.
+//  and adds it into your `topics` array. Then make a function call 
+//  that takes each topic in the array remakes the buttons on the page.
 
 $(document).ready(function() {
-	var dinoMoving = [];
-	var dinoStill = [];
+    var dinoMoving = [];
+    var dinoStill = [];
     var dinoTopics = ["tyrannosaurus", "brachiosaurus", "brontosaurus", "velociraptor", "liopleurodon", "triceratops", "stegosaurus", "iguanodon"];
-    
+
     console.log(dinoTopics);
 
-    function showButtons () {
-    	$("#dino-div").empty();
+    function showButtons() {
+        $("#dino-div").empty();
 
-    	for (var i = 0; i < dinoTopics.length; i++) {
+        for (var i = 0; i < dinoTopics.length; i++) {
 
-    		var createButton = $("<button>");
+            var createButton = $("<button>");
 
-    		createButton.addClass("btn btn-info dino-btn");
+            createButton.addClass("btn btn-info dino-btn");
 
-    		createButton.attr("data-value", dinoTopics[i]);
+            createButton.attr("data-value", dinoTopics[i]);
 
-    		createButton.text(dinoTopics[i]);
+            createButton.text(dinoTopics[i]);
 
-    		$("#dino-div").append(createButton);
-    	};
+            $("#dino-div").append(createButton);
+        };
     };
 
     showButtons();
 
-    function addButton () {
-    	// event.preventDefault();
-    	var newDino = $("#dino-input").val().trim();
-    	if (newDino !== "") {
-    		dinoTopics.push(newDino);
-    		console.log("newDino: " + newDino)
-    		showButtons();
-    	} else {
-    		alert("You forgot your dinosaur.")
-    	}	
-    	$("#dino-input").val("");
-    	return false;
+    function addButton() {
+        // event.preventDefault();
+        var newDino = $("#dino-input").val().trim();
+        if (newDino !== "") {
+            dinoTopics.push(newDino);
+            console.log("newDino: " + newDino)
+            showButtons();
+        } else {
+            alert("You forgot your dinosaur.")
+        }
+        $("#dino-input").val("");
+        return false;
     }
 
     $("#add-dino").click(addButton);
 
 
 
-    
 
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search";
 
-    $.ajax({
-    	url: queryURL,
-    	method: 'GET',
-        data: {
-        	api_key: "dc6zaTOxFJmzC",
-        	q: newDino;
-        	limit: 10
-        }
-    }).done(function(response) {
-        
-    });
+    function dinoGifs() {
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search";
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET',
+            data: {
+                api_key: "dc6zaTOxFJmzC",
+                q: newDino;
+                limit: 10
+            }
+        }).done(function(response) {
+
+        });
+
+    }
+
 
 });
